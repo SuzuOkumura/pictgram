7e21a3a28a14223f0143b66715e855aaca1de7cf
SuzuOkumura <44101bwdive@gmail.com>
2020-05-13 16:47:48 +0000

    piktgram03

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  
+  validates :name, presence: true, length: { maximum: 14 }
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX }
+  validates :password, format: {with: /\A[a-zA-Z0-9]+\z/}, length: { in: 8..32 }
   has_secure_password
 end
