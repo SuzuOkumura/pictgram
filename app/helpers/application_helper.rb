180a62e15773a8699c32fa8f646bbbf8fb5e6d97
SuzuOkumura <44101bwdive@gmail.com>
2020-05-17 07:01:55 +0000

    piktgram06

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
-	def current_user
-		@current_user || user.find_by(id: session[:user_id])
-	end
-	
-	def logged_in?
-		!current_user.nill?
-	end
+
+  include CommonActions
+  
 end
